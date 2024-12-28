8fe9a2e09333902925527d2e608ad8df8f97ceb3
Mizking-Kobayashi <kobayashi-m-ia-40hs@g.bears.ed.jp>
2024-12-28 16:02:40 +0900

    feat:Add new folder.

---
+++ Xcode-AI-MINT-forViT/Xcode-AI-MINT-forViTTests/Xcode_AI_MINT_forViTTests.swift
@@ -0,0 +1,17 @@
+//
+//  Xcode_AI_MINT_forViTTests.swift
+//  Xcode-AI-MINT-forViTTests
+//
+//  Created by Mizking-Kobayashi on 2024/12/19.
+//
+
+import Testing
+@testable import Xcode_AI_MINT_forViT
+
+struct Xcode_AI_MINT_forViTTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
